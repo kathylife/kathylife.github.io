e89c52419ff71cf8f98f3ca61879c1d6cbd1b25d
kathfinder <>
2020-12-13 17:32:26 +0800

    updated blog

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,80 +2,80 @@ var store = [{
         "title": "Welcome to My New Blog",
         "excerpt":"In the past 3 years or so, I have been fortunate enough to garner a humble readership on Medium, publishing both technical and non-technical posts alike. As pleasant to use as Medium has been, I have decided to once again move to a self-hosted blog. That way I get to...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/07/14/welcome-to-my-new-blog/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/07/14/welcome-to-my-new-blog/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Life is Too Short to Build Boring Things",
         "excerpt":"I have updated the About section of my website a little bit. It now includes some of the questions that I constantly ask myself and reflect upon: Are you bored by what you do on a daily basis? Can the boring tasks be handled by robots instead? Can YOU contribute...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/07/21/life-is-too-short-to-build-boring-things/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/07/21/life-is-too-short-to-build-boring-things/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "All Things in Moderation",
         "excerpt":"Late-night ramblings: Code committed and pushed. I almost forgot how fun it was to work directly with raw data bytes that hardware devices spit out. Except instead of gaming consoles, the development work is now done for healthcare robotics. There’s just something about being able to see everything clearly in...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/07/25/all-things-in-moderation/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/07/25/all-things-in-moderation/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Trust Your Gut",
         "excerpt":"In the tail end of 2018, I built a prototype for a robot photographer. The goal was for it to one day serve as a photographer’s stunt double, so that photojournalists won’t always have to put themselves in danger’s way in chaotic zones. The further vision was to repurpose the...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/07/26/trust-your-gut/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/07/26/trust-your-gut/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Likelihood",
         "excerpt":"I could tell something must have gone wrong. The last stretches of the highway leading up to Tai Lam Tunnel — including the 3.8-kilometer tunnel itself — were brought to a complete standstill. As it turns out, there was a crash involving two double-decker buses, injuring 77 people. Before I...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/07/31/likelihood/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/07/31/likelihood/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Acknowledge Its Existence",
         "excerpt":"Today’s late-night ramblings are in regard to something that one cannot see, but undoubtedly exists nonetheless. Are you thinking what I was thinking? I was going to say — blind spot; or perhaps more than one of them. Take driving. It’s possible that there will be multiple blind spots at...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/08/16/acknowledge-its-existence/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/08/16/acknowledge-its-existence/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Stand Up by Sitting Down",
         "excerpt":"I am not a historian, but I read every now and then. Jim Crow laws were a series of American statutes that legalized racial segregation. Existing for about 100 years from as early as 1865 until 1968, the laws were intended to “restore” Southern states to a pre-war class structure...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/08/24/stand-up-by-sitting-down/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/08/24/stand-up-by-sitting-down/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Point A to Point B",
         "excerpt":"Just how long is 50 meters? Let’s not dwell on the “what happened” for now, but I just found myself going down a mind-numbing rabbit hole that is having to untangle a 50m ethernet cable. I *had* to think about something else in the meantime to prevent myself from going...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/08/31/point-a-to-point-b/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/08/31/point-a-to-point-b/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Infinite Power",
         "excerpt":"Coal, petroleum, natural gas… You probably guessed it: I am naming common examples of finite energy resources — finite being the operative word. Once any of these is used up, there will be no way to replenish it. What’s gone is gone. On the other end of the renewability spectrum,...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/09/15/infinite-power/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2019/09/15/infinite-power/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Data Science Can Only Help So Much",
         "excerpt":"Data science is now a lot more mature than it was back in 2003 (the year when the SARS epidemic struck). Understandably, big data is already playing a big role in helping to fight infectious diseases like Ebola and polio. How can we leverage data science advancements to speed up...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/01/26/data-science-can-only-help-so-much/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2020/01/26/data-science-can-only-help-so-much/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Communication Between Someone Blind and Someone Deaf",
         "excerpt":"Year 2020 has NOT been off to a good start for most of the world, which is why I am now more motivated than ever to squeeze my finite time on Earth to create useful tech. What I am currently working on, is an app that lets a deaf person...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/02/17/communication-between-someone-blind-and-someone-deaf/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2020/02/17/communication-between-someone-blind-and-someone-deaf/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Level Up",
         "excerpt":"We will have leveled up when this is all over.   It’s more than just an involuntary lockdown. Think of this period of trying times as a secret training camp — from which we will emerge stronger, more intelligent, more compassionate, and with slightly longer hair.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/03/25/level-up/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2020/03/25/level-up/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Drones",
         "excerpt":"Sometimes we are like drones. Bringing others down won’t make you fly higher.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/03/25/drones/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2020/03/25/drones/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Prototyping Paradigm Shift",
         "excerpt":"As someone who is actively developing a series of solutions to help fight against COVID-19, I think it’s safe to say that the global crisis presents a paradigm shift in the way we prototype. Amidst the pandemic, the questions we should now be asking ourselves are: 1. First and foremost, how...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/04/07/prototyping-paradigm-shift/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2020/04/07/prototyping-paradigm-shift/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "Respect",
         "excerpt":"Part of my ethos is to treat everyone respectfully, regardless of whether or not they are successful. I have tremendously more respect for people who: have fought against hardships throughout their lives have overcome personal struggles have the audacity to take responsibility for past mistakes set goals, failed (perhaps even...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/11/10/respect/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"},{
+        "url": "https://kathy.life/2020/11/10/respect/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
         "title": "History",
         "excerpt":"History has repeatedly told us not to repeat it.   We have been historically terrible at learning from what history tells us.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2020/12/13/history/",
-        "teaser":"http://localhost:4000/assets/images/kathy-life-cover.png"}]
+        "url": "https://kathy.life/2020/12/13/history/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"}]
