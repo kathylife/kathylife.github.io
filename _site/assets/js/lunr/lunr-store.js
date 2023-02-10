ce70da242a5a8b8f26be480e1a1054181bbb3c19
kathfinder <syli@cs.washington.edu>
2023-02-11 05:33:12 +0800

    added new haiku

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,196 @@
+var store = [{
+        "title": "Welcome to My New Blog",
+        "excerpt":"In the past 3 years or so, I have been fortunate enough to garner a humble readership on Medium, publishing both technical and non-technical posts alike. As pleasant to use as Medium has been, I have decided to once again move to a self-hosted blog. That way I get to...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/welcome-to-my-new-blog/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Life is Too Short to Build Boring Things",
+        "excerpt":"I have updated the About section of my website a little bit. It now includes some of the questions that I constantly ask myself and reflect upon: Are you bored by what you do on a daily basis? Can the boring tasks be handled by robots instead? Can YOU contribute...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/life-is-too-short-to-build-boring-things/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "All Things in Moderation",
+        "excerpt":"Late-night ramblings: Code committed and pushed. I almost forgot how fun it was to work directly with raw data bytes that hardware devices spit out. Except instead of gaming consoles, the development work is now done for healthcare robotics. There’s just something about being able to see everything clearly in...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/all-things-in-moderation/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Trust Your Gut",
+        "excerpt":"In the tail end of 2018, I built a prototype for a robot photographer. The goal was for it to one day serve as a photographer’s stunt double, so that photojournalists won’t always have to put themselves in danger’s way in chaotic zones. The further vision was to repurpose the...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/trust-your-gut/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Likelihood",
+        "excerpt":"I could tell something must have gone wrong. The last stretches of the highway leading up to Tai Lam Tunnel — including the 3.8-kilometer tunnel itself — were brought to a complete standstill. As it turns out, there was a crash involving two double-decker buses, injuring 77 people. Before I...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/likelihood/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Acknowledge Its Existence",
+        "excerpt":"Today’s late-night ramblings are in regard to something that one cannot see, but undoubtedly exists nonetheless. Are you thinking what I was thinking? I was going to say — blind spot; or perhaps more than one of them. Take driving. It’s possible that there will be multiple blind spots at...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/acknowledge-its-existence/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Stand Up by Sitting Down",
+        "excerpt":"I am not a historian, but I read every now and then. Jim Crow laws were a series of American statutes that legalized racial segregation. Existing for about 100 years from as early as 1865 until 1968, the laws were intended to “restore” Southern states to a pre-war class structure...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/stand-up-by-sitting-down/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Point A to Point B",
+        "excerpt":"Just how long is 50 meters? Let’s not dwell on the “what happened” for now, but I just found myself going down a mind-numbing rabbit hole that is having to untangle a 50m ethernet cable. I *had* to think about something else in the meantime to prevent myself from going...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/point-a-to-point-b/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Infinite Power",
+        "excerpt":"Coal, petroleum, natural gas… You probably guessed it: I am naming common examples of finite energy resources — finite being the operative word. Once any of these is used up, there will be no way to replenish it. What’s gone is gone. On the other end of the renewability spectrum,...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/infinite-power/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Data Science Can Only Help So Much",
+        "excerpt":"Data science is now a lot more mature than it was back in 2003 (the year when the SARS epidemic struck). Understandably, big data is already playing a big role in helping to fight infectious diseases like Ebola and polio. How can we leverage data science advancements to speed up...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/data-science-can-only-help-so-much/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Communication Between Someone Blind and Someone Deaf",
+        "excerpt":"Year 2020 has NOT been off to a good start for most of the world, which is why I am now more motivated than ever to squeeze my finite time on Earth to create useful tech. What I am currently working on, is an app that lets a deaf person...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/communication-between-someone-blind-and-someone-deaf/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Level Up",
+        "excerpt":"We will have leveled up when this is all over.   It’s more than just an involuntary lockdown. Think of this period of trying times as a secret training camp — from which we will emerge stronger, more intelligent, more compassionate, and with slightly longer hair.  ","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/level-up/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Drones",
+        "excerpt":"Sometimes we are like drones. Bringing others down won’t make you fly higher.  ","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/drones/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Prototyping Paradigm Shift",
+        "excerpt":"As someone who is actively developing a series of solutions to help fight against COVID-19, I think it’s safe to say that the global crisis presents a paradigm shift in the way we prototype. Amidst the pandemic, the questions we should now be asking ourselves are: 1. First and foremost, how...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/prototyping-paradigm-shift/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Respect",
+        "excerpt":"Part of my ethos is to treat everyone respectfully, regardless of whether or not they are successful. I have tremendously more respect for people who: have fought against hardships throughout their lives have overcome personal struggles have the audacity to take responsibility for past mistakes set goals, failed (perhaps even...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/respect/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "History",
+        "excerpt":"History has repeatedly told us not to repeat it.   We have been historically terrible at learning from what history tells us.  ","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/history/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Exit",
+        "excerpt":"Getting a rejection is like taking an early exit on the freeway. You are not necessarily changing your destination; perhaps you are just taking a different route to get there.  ","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/exit/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Milkweed",
+        "excerpt":"Most milkweed flowers are poisonous to humans and pets, but at the same time can be a great nectar source for pollinators like butterflies and honeybees — *if* they don’t get trapped inside and die. Be yourself, and the right people will come by at some point or another. Those...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/milkweed/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Street Signs",
+        "excerpt":"[Kingston Street, Causeway Bay, 2022] The street signs in Hong Kong — as compact as they tend to be in size — subtly pack a lot of useful information. Take this one in the picture. The straight edge on the left of the sign tells us that there is no...","categories": ["blog"],
+        "tags": [],
+        "url": "https://kathy.life/blog/street-signs/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "If You Name It",
+        "excerpt":"When we give our pet project a name and identity, we become that much more attached to it and, in turn, may just have a higher likelihood of seeing it through. The New Project Earlier in the month I got an idea to start “Project NFT Inception” — a project...","categories": ["final-static"],
+        "tags": [],
+        "url": "https://kathy.life/final-static/if-you-name-it/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "How to Hook Up Custom Domain Emails to Free Gmail Account",
+        "excerpt":"In the previous blog post, I had signed up for a free Gmail account to manage emails for the Final Static project. As a follow-up, I am going to document the steps I took to connect it with the custom domain I had newly purchased. Under this setup, I would...","categories": ["final-static"],
+        "tags": [],
+        "url": "https://kathy.life/final-static/set-up-custom-domain-emails-free/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Configuring Development Environment to Build Full-Stack Dapps",
+        "excerpt":"One nice thing about building full-stack applications from scratch is the flexibility. You are not limited to any one fixed set of tools, and can therefore go the pragmatic route of selecting: a. What suits the job best b. What you are most familiar with c. A mix of the...","categories": ["final-static"],
+        "tags": [],
+        "url": "https://kathy.life/final-static/set-up-environment-to-build-full-stack-dapp/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Dapp Project Setup",
+        "excerpt":"In the previous post I went through the steps to configure my local development environment, so now would be a good time to start creating the project skeleton. Creating a Next.js project As a memory refresher: Next.js is an open-source web development framework built on top of Node.js, enabling React-based...","categories": ["final-static"],
+        "tags": [],
+        "url": "https://kathy.life/final-static/dapp-project-setup/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Implementing the Smart Contract",
+        "excerpt":"As documented in the previous post, I built out the application skeleton which came with an auto-generated sample Solidity smart contract. A smart contract is a self-executing protocol with the terms of the agreement between two parties (e.g. buyer and seller) being directly written into lines of code. The code...","categories": ["final-static"],
+        "tags": [],
+        "url": "https://kathy.life/final-static/implementing-smart-contract/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Building the Front End",
+        "excerpt":"Previously for Final Static, I built and tested the NFT marketplace’s smart contract. To support user interactions that work with it, the next important step would be to build out the web-based front end. This was where the Tailwind CSS setup came in handy as well. Building the navigation bar...","categories": ["final-static"],
+        "tags": [],
+        "url": "https://kathy.life/final-static/building-front-end/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #1",
+        "excerpt":"Life is but a dream   When the stomach demands cake   I know I’m awake  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/1/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #2",
+        "excerpt":"Ran with all my strength   Harsh lull at the finish line   Next bus comes in nine  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/2/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #3",
+        "excerpt":"Light became bright red   Reshaping all lights ahead   Rushed driver saw red  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/3/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #4",
+        "excerpt":"She was on the phone   Neglecting not just the honk   But life of her own  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/4/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #5",
+        "excerpt":"Light years might be far   Well-thought-out goals can be reached   As can that snack jar  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/5/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #6",
+        "excerpt":"Sometimes the sky’s gray   It can’t be blue every day   Just go out and play  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/6/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #7",
+        "excerpt":"Napping on the bus   Woken up by a man’s cough   Right where I’d get off  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/7/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #8",
+        "excerpt":"Large crowds in the streets   Twelve thousand destinations   Twelve constellations  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/8/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #9",
+        "excerpt":"Year of the Rabbit   Could mark a perfect start — to   Bury the hatchet   * * * * * * * * * *   Version 2.0:   Year of the Rabbit   Could mark a perfect start — to   Form a new habit  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/9/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #10",
+        "excerpt":"Believe in yourself   For no one will, if you don’t   Believe in yourself  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/10/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #11",
+        "excerpt":"Quaoar has a ring   That perplexes everyone   I do not have one  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/11/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #12",
+        "excerpt":"Something of a coup   Did you happen to notice   This is a haiku  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/12/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Haiku #13",
+        "excerpt":"Stopped at a crossroads   Most people know where they’ll go   Some need an arrow  ","categories": ["haiku"],
+        "tags": [],
+        "url": "https://kathy.life/haiku/13/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"},{
+        "title": "Chocolate",
+        "excerpt":"Chocolate cupcake   ----------   Source: none  ","categories": [],
+        "tags": [],
+        "url": "https://kathy.life/some-collection/chocolate/",
+        "teaser":"https://kathy.life/assets/images/kathy-life-cover.png"}]
